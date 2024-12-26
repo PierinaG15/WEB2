eeb025b46876f5ed5578fd089c160bfed0183776
PierinaG15 <pierinaagh7@gmail.comecho>
2024-12-26 20:18:05 +0100

    finalizado trabajo del dia web2

---
+++ js/registro.js
@@ -0,0 +1,21 @@
+var inputPass = document.getElementById("password");
+var inputRePass = document.getElementById("repassword");
+var btnCrear = document.getElementById("btnCrear");
+var msg=document.getElementById("msg")
+
+inputPass.oninput=validacionInput;
+inputRePass.oninput=validacionInput
+
+function validacionInput() {
+    var txtPass = inputPass.value;
+    var txtRePass = inputRePass.value;
+    if (txtPass == txtRePass) {
+        btnCrear.disabled=false;
+        msg.style.display="none"
+    }else{  
+         btnCrear.disabled=true;
+         msg.style.display="block"
+        }
+     
+       // console.log(txtPass);
+    }
